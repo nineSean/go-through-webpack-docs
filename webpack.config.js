594cc3c2696b8376c55f60b7b7cd1fdf5bbb7ff1
nineSean <ninesean1989@gmail.com>
2019-02-22 10:05:49 +0800

    preparation for output management

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    index: './src/index.js'
+    app: './src/index.js',
+    print: './src/print.js'
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
 }
