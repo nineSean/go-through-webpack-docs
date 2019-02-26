fc546e84a8ddcadb5fb22c142c966204cde47210
nineSean <ninesean1989@gmail.com>
2019-02-26 11:19:16 +0800

    module identifiers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
@@ -15,7 +16,8 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HTMLWebpackPlugin({
       title: 'caching',
-    })
+    }),
+    new webpack.HashedModuleIdsPlugin()
   ],
   optimization: {
     runtimeChunk: 'single',
